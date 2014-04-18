9754e217d8ec203d5ee2659a9202be6471f50964
Sylvain Rayé <sly@diglin.com>
2014-04-18 21:10:45 +0200

    finish to implement the overrides to add google calendar checkbox

--- DevFineGoogleCalendarEventsBundle.php
+++ DevFineGoogleCalendarEventsBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DevFineGoogleCalendarEventsBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'OroCalendarBundle';
+    }
 }
